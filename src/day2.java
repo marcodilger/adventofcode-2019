add15fd5d4a76802ba6c8908a54f4884c58ca064
Marco Dilger <marcodilger@gmx.de>
2019-12-02 18:21:52 +0100

    solved day 2 part 2

--- src/day2.java
+++ src/day2.java
@@ -18,6 +18,9 @@ public class day2 {
                 ops[i] = Integer.parseInt(s);
                 i++;
             }
+            
+            //part 1
+            
             // custom modifications as per description:
             ops[1] = 12;
             ops[2] = 02;
@@ -41,7 +44,52 @@ public class day2 {
 //                    System.out.println(k);
 //                }
             }
-            System.out.println("result day 1 part 1: " + ops[0]); // should work for part 1
+            System.out.println("result day 1 part 1: " + ops[0]); // solution for part 1
+            
+            
+            
+            // part 2
+            
+            //solve by bruteforcing with 2 loops
+            
+            for (int noun = 0; noun <= 99; noun++) {
+                for (int verb = 0; verb <= 99; verb++) {
+            // start fresh each iteration
+            i = 0;
+            for (String s : opsString) {
+                ops[i] = Integer.parseInt(s);
+                i++;
+            }
+            
+            ops[1] = noun;
+            ops[2] = verb;
+            
+            System.out.println("testing noun verb:" + (noun * 100 + verb));
+            
+            // run op code
+            breakFound = false;
+            while (breakFound == false) {
+            
+                for (int op = 0; op <= opsString.length; op = op + 4) { // op is equivalent to the instructions pointer introduced in part 2
+                    if (ops[op] == 99) {
+                        breakFound = true;
+                        break; // this round is over
+                    } else if (ops[op] == 1) { // addition
+                        ops[ops[op + 3]] = ops[ops[op + 1]] + ops[ops[op + 2]];
+                    } else if (ops[op] == 2) { // multiplication
+                        ops[ops[op + 3]] = ops[ops[op + 1]] * ops[ops[op + 2]];
+                    }
+                }
+                    
+                }
+                // check if correct
+                if (ops[0] == 19690720) {
+                    System.out.println("result day2 part 2: " + (noun * 100 + verb)); // correct
+                    noun = 100;
+                    verb = 100;
+                }
+            }
 
+        } // end outer loop
         }
 }
