b866cfc1e0883cd9b3b17fba54f73ffdddf02aed
Marco Dilger <marcodilger@gmx.de>
2019-12-04 17:14:35 +0100

    day 4 part2

--- src/day4.java
+++ src/day4.java
@@ -9,26 +9,32 @@
  * @author marco
  */
 public class day4 {
+
     public static void main(String[] args) {
         //bruteforce
-        
+
         int[] codeArray = new int[6];
         int counter = 0;
         int start = 264793;
         int end = 803935;
-        
-        for (int code = start; code <= end; code++){
+
+        for (int code = start; code <= end; code++) {
             int temp = code;
             for (int i = 5; i >= 0; i--) {
                 codeArray[i] = temp % 10;
                 temp = temp / 10;
-               }
-            
+            }
+
             //validity checks
             boolean containsDouble = false;
             boolean strictlyIncrementing = true;
-            
+
             // check for doubles
+            // probably better, especially with part2 woudl have been:
+            // assert tht array is strictly incrementing,
+            // then just count digits,
+            // part1: at least 1 digit > 2 times
+            // part2: at least 1 digit ecactly 2 times
             for (int i = 0; i < 5; i++) {
                 if (codeArray[i] == codeArray[i + 1]) {
                     containsDouble = true;
@@ -37,19 +43,51 @@ public class day4 {
             }
             // check for strictly incrementing
             if (containsDouble == true) {
-                    for (int k = 0; k < 5; k++) {
-                        if (codeArray[k] > codeArray[k + 1]) {
-                            strictlyIncrementing = false;
+                for (int k = 0; k < 5; k++) {
+                    if (codeArray[k] > codeArray[k + 1]) {
+                        strictlyIncrementing = false;
+                        break;
+                    }
+                }
+                if (strictlyIncrementing) {
+                    // counter++;  // part 1
+                    // additional requirement for part 2:
+                    // there needs to be a shortest chain of adjacent same digits
+
+                    boolean containsStrictDouble = false;
+                    for (int i = 0; i <= 9; i++) {
+
+                        if (chainExactlyTwo(i, codeArray)) {
+                            counter++;
                             break;
                         }
                     }
-                if (strictlyIncrementing) {
-                    counter++;
                 }
             }
         }
-        System.out.println("result day 2 part 1: " + counter);
-                
+        System.out.println("result day 2 part 2: " + counter);
+
     }
-    
+
+    public static boolean chainExactlyTwo(int digit, final int[] array) { // checks for a single digit, if there is a consecutive chain of same digits which is longer than 2
+        int max = array.length;
+        boolean found2 = false;
+        boolean found3 = false;
+        for (int i = 0; i < 5; i++) {
+            if (array[i] == digit
+                    & array[i + 1] == digit) {
+                found2 = true;
+                break;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            if (array[i] == digit
+                    & array[i + 1] == digit
+                    & array[i + 2] == digit) {
+                found3 = true;
+                break;
+            }
+        }
+        return (found2 & !found3); // returns true only if 2 ha sbeen found but not 3
+    }
 }
